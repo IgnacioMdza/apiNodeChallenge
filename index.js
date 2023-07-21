e3ca6ccf1163dd0957aba6ebf11f8bf15d4d71a9
IgnacioMdza <mdza.ignacio@gmail.com>
2023-07-20 21:01:02 -0600

    added process.env.PORT

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const databaseUrl = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
+const port = process.env.PORT || 8080
+
 mongoose
     .connect(databaseUrl)
     .then(() => {
         console.log("Conexión a la base de datos exitosa");
-        app.listen(8080, () => {
-            console.log("La api está prendida!");
+        app.listen(port, () => {
+            console.log("Server listening on port:", port);
         });
         // Levantar servidor
     })
     .catch((err) => {
         console.log("No se pudo conectar a la base de datos");
     });
-    
